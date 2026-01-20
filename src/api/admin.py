8abce952bcb2ef8823830fefef5c6389eb66520b
AEGiner <andresesquivel2011@gmail.com>
2026-01-20 00:10:52 +0000

    Added models for Users, Pets and Doctors

--- src/api/admin.py
+++ src/api/admin.py
@@ -2,17 +2,34 @@ import os
 import inspect
 from flask_admin import Admin
 from . import models
-from .models import db
+from .models import db, Users, Pets, Doctors
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.theme import Bootstrap4Theme
+
+
+class UserModelView(ModelView):
+    column_auto_select_related=True
+    column_list = ['user_id','first_name','last_name','email','phonenumber','address','password','pets']
+
+class PetsModelView(ModelView):
+    column_auto_select_related=True
+    column_list = ['pet_id','owner_id','owner','name','birthdate','breed','allergies','neutered','info','image']
+
+class DoctorsModelView(ModelView):
+    column_auto_select_related=True
+    column_list = ['doctor_id','first_name','last_name','specialty','email','phonenumber','password']
 
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     admin = Admin(app, name='4Geeks Admin', theme=Bootstrap4Theme(swatch='cerulean'))
 
-    # Dynamically add all models to the admin interface
-    for name, obj in inspect.getmembers(models):
-        # Verify that the object is a SQLAlchemy model before adding it to the admin. 
-        if inspect.isclass(obj) and issubclass(obj, db.Model):
-            admin.add_view(ModelView(obj, db.session))
+    admin.add_view(UserModelView(Users, db.session))
+    admin.add_view(PetsModelView(Pets, db.session))
+    admin.add_view(DoctorsModelView(Doctors, db.session))
+
+    # # Dynamically add all models to the admin interface
+    # for name, obj in inspect.getmembers(models):
+    #     # Verify that the object is a SQLAlchemy model before adding it to the admin. 
+    #     if inspect.isclass(obj) and issubclass(obj, db.Model):
+    # admin.add_view(ModelView(obj, db.session))
