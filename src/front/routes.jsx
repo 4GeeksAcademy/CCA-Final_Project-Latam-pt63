426c3c3ba6a65432ba9abc0d319277365a986915
zuletacarlos <carlosluiszuleta@yahoo.es>
2026-01-27 01:34:57 +0000

    register pet terminado cloudinary funcionando

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,5 +1,4 @@
 // Import necessary components and functions from react-router-dom.
-
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -11,31 +10,28 @@ import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
-import { Homepage } from "./pages/Homepage"
+import { Homepage } from "./pages/Homepage";
 import { Services } from "./pages/Services";
-import { About } from "./pages/About"
+import { About } from "./pages/About";
 import { Profile } from "./pages/Profile";
+import { RegisterPet } from "./pages/RegisterPet.jsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    // CreateRoutesFromElements function allows you to build route elements declaratively.
-    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
-    // Root, on the contrary, create a sister Route, if you have doubts, try it!
-    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
-    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
-
-    // Root Route: All navigation will start from here.
     <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
-      {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
+      { }
       <Route path="/" element={<Homepage />} />
-      <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
+      <Route path="/single/:theId" element={<Single />} />
       <Route path="/demo" element={<Demo />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/services" element={<Services />} />
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />
+      { }
+      <Route path="/register-pet" element={<RegisterPet />} />
+
     </Route>
   )
 );
