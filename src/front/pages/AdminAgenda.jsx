ef1d13f4b8ca861350ad3b01f7b075a6aedc58a5
AEGiner <andresesquivel2011@gmail.com>
2026-02-12 00:12:53 +0000

    Changed spinner color

--- src/front/pages/AdminAgenda.jsx
+++ src/front/pages/AdminAgenda.jsx
@@ -170,7 +170,7 @@ export const AdminAgenda = () => {
   if (!appointments) {
     return (
       <div className="d-flex justify-content-center align-items-center min-vh-100">
-        <div className="spinner-border text-success" role="status">
+        <div className="spinner-border " role="status" style={{ color: "rgb(48, 130, 114)", width: "3rem", height: "3rem" }}>
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
--- src/front/pages/AdminClients.jsx
+++ src/front/pages/AdminClients.jsx
@@ -96,7 +96,7 @@ export const AdminClients = () => {
   if (!clients) {
     return (
       <div className="d-flex justify-content-center align-items-center min-vh-100">
-        <div className="spinner-border text-success" role="status">
+        <div className="spinner-border " role="status" style={{ color: "rgb(48, 130, 114)", width: "3rem", height: "3rem" }}>
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
